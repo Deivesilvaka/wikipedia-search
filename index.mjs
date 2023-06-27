953f5177e3333ec3edb223c2ba3e5678c7e864d2
Luiz Davi da Silva Freitas <luizdaviinformatica@gmail.com>
2023-06-27 09:39:25 -0300

    feat: new repo

---
+++ index.mjs
@@ -0,0 +1,76 @@
+import wikipedia from 'wikipedia'
+import sentenceBoundaryDetection  from 'sbd'
+
+export class Wikipedia {
+
+    #sentencesQuantity = 0
+
+    constructor(query, sentencesQuantity = 10) {
+        this.query = query
+        this.description = ''
+        this.content = {}
+        this.#sentencesQuantity = sentencesQuantity
+        this.sentences = []
+    }
+
+    #breakContentIntoSentences(content) {
+        const sentences = sentenceBoundaryDetection.sentences(content)
+        
+        for(let sentence = 0; sentence <= this.#sentencesQuantity; sentence++){
+            this.sentences.push(sentences[sentence])
+        }
+        return
+    }
+
+    #removeBlankLinesAndMarkDown(text) {
+    
+        const allLines = text.split('\n')
+    
+        const withoutBlankLinesAndMarkDown = allLines.filter((line) => {
+            if(line.trim().length === 0 || line.trim().startsWith('=')){
+                return false
+            }
+            return true
+        })
+        return this.#removeDateInParenteses(withoutBlankLinesAndMarkDown.join(' '))
+    }
+
+    #removeDateInParenteses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+    }
+
+    async search() {
+        try {
+            const apiContent = await wikipedia.page(this.query, {
+                autoSuggest: true,
+            })
+        
+            const [intro, images, summary, content, references] = await Promise.all([
+                apiContent.intro(),
+                apiContent.images(),
+                apiContent.summary(),
+                apiContent.content(),
+                apiContent.references()
+            ])
+            
+            this.description = summary.description
+
+            const sanitizedContent = this.#removeBlankLinesAndMarkDown(content)
+            this.#breakContentIntoSentences(sanitizedContent)
+    
+            this.content = {
+                intro,
+                images,
+                summary: this.#removeBlankLinesAndMarkDown(summary.extract),
+                content: sanitizedContent,
+                references
+            }
+    
+            return this
+        } catch (err) {
+            return {
+                error: 'Objeto não encontrado'
+            }
+        }
+    }
+}
